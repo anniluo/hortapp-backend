c204bec3d1ea60a1fea2af0b77db32eeb5994754
anniluo <anniluo@metropolia.fi>
2020-04-08 19:04:47 +0300

    ui build

---
+++ build/precache-manifest.e861ce9eadec027d6639579964e5c95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f0ba1169fb878a94c18fba5f8a3412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd12d4c056ae9f44e0f2",
+    "url": "/static/css/main.8b54ff3e.chunk.css"
+  },
+  {
+    "revision": "c721071cd31232758980",
+    "url": "/static/js/2.13831352.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.13831352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd12d4c056ae9f44e0f2",
+    "url": "/static/js/main.8e9908d3.chunk.js"
+  },
+  {
+    "revision": "01a0ee700afbc7d49b81",
+    "url": "/static/js/runtime-main.a94cd276.js"
+  }
+]);
