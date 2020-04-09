a133c71506daa4a4744a6f97f29833cee86889d2
anniluo <anniluo@metropolia.fi>
2020-04-09 15:42:59 +0300

    uibuild

---
+++ build/precache-manifest.22c41014d1b343c184f8b400322ec490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3234096e28ea06ff4f55ba058711345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "597b286683d87f589257",
+    "url": "/static/css/main.da2eb50a.chunk.css"
+  },
+  {
+    "revision": "9f7f999f485c416ab624",
+    "url": "/static/js/2.c56442cf.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.c56442cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597b286683d87f589257",
+    "url": "/static/js/main.b42f1b35.chunk.js"
+  },
+  {
+    "revision": "01a0ee700afbc7d49b81",
+    "url": "/static/js/runtime-main.a94cd276.js"
+  },
+  {
+    "revision": "70b251c3211d8cb1b097176e32abcd0c",
+    "url": "/static/media/greens-icon.70b251c3.png"
+  }
+]);
